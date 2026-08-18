d2b8e05caa696d0d9b389a9cb2b4cfb17825c995
Giandomenico Olini <giando.olini@gogodigital.it>
2026-08-18 20:27:06 +0200

    Add FullCalendar asset bundle

---
+++ CalendarAsset.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace cinghie\adminlte;
+
+use yii\web\AssetBundle;
+use yii\web\YiiAsset;
+
+/**
+ * FullCalendar assets bundled with AdminLTE 2.
+ */
+class CalendarAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/almasaeed2010/adminlte/';
+
+    public $css = [
+        'bower_components/fullcalendar/dist/fullcalendar.min.css',
+    ];
+
+    public $js = [
+        'bower_components/jquery-ui/jquery-ui.min.js',
+        'bower_components/moment/moment.js',
+        'bower_components/fullcalendar/dist/fullcalendar.min.js',
+    ];
+
+    public $depends = [
+        YiiAsset::class,
+    ];
+}

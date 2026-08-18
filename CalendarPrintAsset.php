9fb8f72ed792eb23b237f6d45792c16a44672e1b
Giandomenico Olini <giando.olini@gogodigital.it>
2026-08-18 20:30:59 +0200

    Add FullCalendar print asset

---
+++ CalendarPrintAsset.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace cinghie\adminlte;
+
+use yii\web\AssetBundle;
+
+/**
+ * FullCalendar print stylesheet bundled with AdminLTE 2.
+ */
+class CalendarPrintAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/almasaeed2010/adminlte/';
+
+    public $css = [
+        'bower_components/fullcalendar/dist/fullcalendar.print.min.css',
+    ];
+
+    public $cssOptions = [
+        'media' => 'print',
+    ];
+
+    public $depends = [
+        CalendarAsset::class,

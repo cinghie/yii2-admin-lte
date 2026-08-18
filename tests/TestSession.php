f7b11c554f020c29ef3b3f3e6e29d817bdc6c370
Giandomenico Olini <giando.olini@gogodigital.it>
2026-08-18 22:29:23 +0200

    Use in-memory session for tests

---
+++ tests/TestSession.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace cinghie\adminlte\tests;
+
+use yii\web\Session;
+
+/**
+ * In-memory session for tests that avoids PHP session headers/ini changes.
+ */
+class TestSession extends Session
+{
+    private $data = [];
+
+    public function getIsActive()
+    {
+        return true;
+    }
+
+    public function open()
+    {
+        // Intentionally no-op: tests do not need a real PHP session.
+    }
+
+    public function close()
+    {
+        // Intentionally no-op.
+    }
+
+    public function get($key, $defaultValue = null)
+    {
+        return array_key_exists($key, $this->data) ? $this->data[$key] : $defaultValue;
+    }
+
+    public function set($key, $value)
+    {
+        $this->data[$key] = $value;
+    }
+
+    public function remove($key)
+    {
+        if (!array_key_exists($key, $this->data)) {
+            return null;
+        }
+
+        $value = $this->data[$key];
+        unset($this->data[$key]);
+
+        return $value;
+    }
+
+    public function removeAll()
+    {
+        $this->data = [];
+    }
+}
